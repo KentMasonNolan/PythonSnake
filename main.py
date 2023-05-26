494a5aef688317691a7411e7554f2f25fd93db9c
kent <kent.nolan@gmail.com>
2023-05-26 15:14:51 +1200

    Setup run time

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
+from snake import *
 
 pygame.init()
-bounds = (300,300)
+bounds = (300, 300)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Snake")
 
+block_size = 20
+snake = Snake(block_size, bounds)
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    window.fill((0, 0, 0))
+    snake.draw(pygame, window)
+    pygame.display.flip()
